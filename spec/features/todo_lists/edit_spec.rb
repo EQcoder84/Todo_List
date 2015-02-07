b819d3eb85c4b50eebf54d269724c9dce828b92f
Enrique Quevedo <efqueved@aol.com>
2015-02-07 17:23:13 -0500

    Add specs for deleting to-dos

--- spec/features/todo_lists/edit_spec.rb
+++ spec/features/todo_lists/edit_spec.rb
@@ -23,11 +23,12 @@ describe "Editing todo lists" do
 						 title: "New title", 
 						 description: "New description"
 		
-		
 		todo_list.reload
 
 		expect(page).to have_content("Todo list was successfully updated")
 		expect(todo_list.title).to eq("New title")
 		expect(todo_list.description).to eq("New description")
 	end	
+
+	it
 end
